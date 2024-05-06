00c579745ecfdc9ba0e17e1acdc71aaae32fd616
Liukaku <46352428+Liukaku@users.noreply.github.com>
2024-05-06 14:18:38 +0100

    i guess this is a start but its a v hacky

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/gopxl/beep"
@@ -12,67 +13,41 @@ import (
 	"github.com/gopxl/beep/speaker"
 )
 
-var song = ""
+var k = ""
 
 // I should really set this up to work with channels but i tried for like an hour and i really cba
 
-// var song = make(chan string)
 
 func main() {
-	// var wg sync.WaitGroup
-	// wg.Add(1)
+	ch := make(chan string, 2)
+	var wg sync.WaitGroup
+	wg.Add(2)
 	// run the music handler on a separate thread/goroutine so the server can intercept requests
-	go keepGoing()
+	go keepGoing(ch, &wg)
 	// <- song
 
+	go runServer(ch, &wg)
 	
-	// wg.Wait()
+	wg.Wait()
 	// http server for getting up to date requests 
-	runServer()
 }
 
-func runServer(){
-	// defer wg.Done()
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("received get request")
-		io.WriteString(w, fmt.Sprintf("Received request: %s \n song number: %s", r.Method, song))
-	})
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-
-func songPrompt() string {
-	fmt.Println("Pick a song number. 1-10")
-	var songNumber string
-	fmt.Scanln(&songNumber)
-	if songNumber == "q" {
-		fmt.Println("You just had to follow the damn train")
-		os.Exit(3)
-	}
-
-	return songNumber
-}
-
-func keepGoing() {
-
+func keepGoing(ch chan string, wg *sync.WaitGroup) {
+	
 	// defer wg.Done()
 	// wg.Wait()
-
-	k := songPrompt()
-	song = k
-
+	
+	songPrompt(ch)
+	k = <- ch
+	
 	fmt.Println("received song")
 	// val, ok := <- song
-
+	
 	// fmt.Println(ok)
 	// fmt.Println(val)
 	// song <- k
-	
-	songToPlay := fmt.Sprintf("./audio/BEATS/Track_00%s.mp3", song)
+	fmt.Printf("song %s", k)
+	songToPlay := fmt.Sprintf("./audio/BEATS/Track_00%s.mp3", k)
 	f, err := os.Open(songToPlay)
 	
 	if err != nil{
@@ -93,13 +68,39 @@ func keepGoing() {
 	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
 		done <- true
 	})))
-
+	
 	// <- song 
-
+	
 	<- done
-
+	
 	// close(song)
-
-	keepGoing()
+	
+	wg.Add(1)
+	keepGoing(ch, wg)
 	// defer streamer.Close()
+}
+
+func runServer(curr <- chan string, wg *sync.WaitGroup){
+	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// wg.Add(1)
+		fmt.Println("received get request")
+		io.WriteString(w, fmt.Sprintf("Received request: %s \n song number: %s", r.Method, k))
+
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func songPrompt(ch chan string) {
+	fmt.Println("Pick a song number. 1-10")
+	var songNumber string
+	fmt.Scanln(&songNumber)
+	if songNumber == "q" {
+		fmt.Println("You just had to follow the damn train")
+		os.Exit(3)
+	}
+	ch <- songNumber
 }
